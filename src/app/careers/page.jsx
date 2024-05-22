329ea82918f82c2c082b1cdc727841c823065811
declan ogbodo <dheklanintech@gmail.com>
2024-05-22 14:45:23 +0100

    commit

--- src/app/careers/page.jsx
+++ src/app/careers/page.jsx
@@ -1,9 +1,5 @@
 'use client'
 import React from "react";
-import React, { useEffect } from "react";
-
-
-
 import NavLight from "../components/navlight";
 import Footer from "../components/footer";
 
